b6648b9c2470189110270b6b8b92f5b25fba9a4f
aidanii <aidanii_real@proton.me>
2026-04-04 10:48:23 +0800

    project: Moved complib compilation instruction to setuptools install and egg_info command

--- core/setup.py
+++ core/setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 from setuptools.command.install import install
-from setuptools.command.build_py import build_py
 from setuptools.command.egg_info import egg_info
 import subprocess
 import platform
@@ -8,7 +7,6 @@ import shutil
 import os
 
 scd: str = os.getenv('scd', os.path.dirname(os.path.abspath(__file__)))
-ltp: str = os.path.join(scd, 'src', 'lib')
 mwd: str = os.path.join(scd, 'src', 'comptoe')
 lbd: str = os.path.join(scd, 'src', 'vesperando_core', 'lib')
 ext: str = ".so"
@@ -18,50 +16,40 @@ if platform.system() == "Windows":
 elif platform.system() == "Darwin":
     ext = ".dylib"
 
-class BuildPySharedLibrary(build_py):
-    @classmethod
-    def build_library(cls):
-        os.makedirs(ltp)
+def _compile_library():
+    if platform.system() == "Windows":
+        subprocess.call(["gcc", "-shared", "-o", "_complib.dll", "complib.c", "-Wl,--export-all-symbols"],
+                        cwd=mwd)
+    else:
+        subprocess.call(["gcc", "-fPIC", "-shared", "-o", f"_complib{ext}", "complib.c"], cwd=mwd)
 
-        cls._clean_library()
-        cls._compile_library()
+def _clean_library():
+    extensions: tuple = ("_*.so", "_*.dll", "_*.dylib")
+    for file in os.listdir(mwd):
+        if file.endswith(extensions):
+            os.remove(os.path.join(lbd, file))
 
-    @staticmethod
-    def _compile_library():
-        if platform.system() == "Windows":
-            subprocess.call(["gcc", "-shared", "-o", "_complib.dll", "complib.c", "-Wl,--export-all-symbols"],
-                            cwd=mwd)
-        else:
-            subprocess.call(["gcc", "-fPIC", "-shared", "-o", f"_complib{ext}", "complib.c"], cwd=mwd)
-
-    @staticmethod
-    def _clean_library():
-        extensions: tuple = ("_*.so", "_*.dll", "_*.dylib")
-        for file in os.listdir(mwd):
-            if file.endswith(extensions):
-                os.remove(os.path.join(lbd, file))
-
-    def run(self):
-        self.build_library()
-        shutil.move(os.path.join(mwd, f"_complib{ext}"), os.path.join(ltp, f"_complib{ext}"))
-        super().run()
-
+def _install_library():
+    shutil.move(os.path.join(mwd, f"_complib{ext}"), os.path.join(lbd, f"_complib{ext}"))
 
 class InstallSharedLibrary(install):
     def run(self):
-        shutil.copytree(ltp, lbd, dirs_exist_ok=True)
+        _clean_library()
+        _compile_library()
+        _install_library()
         super().run()
 
 
 class EggInfoSharedLibrary(egg_info):
     def run(self):
-        shutil.copytree(ltp, lbd, dirs_exist_ok=True)
+        _clean_library()
+        _compile_library()
+        _install_library()
         super().run()
 
 
 setup(
     cmdclass={
-        'build_py': BuildPySharedLibrary,
         'install': InstallSharedLibrary,
         'egg_info': EggInfoSharedLibrary,
     }
