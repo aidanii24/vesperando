47a95f7dd619fc230f35eed6c53b5bae93a991d8
aidanii <aidanii_real@proton.me>
2026-07-23 16:46:59 +0800

    test: Moved restore test to use CLIRunner

--- tests/integration/test_restore.py
+++ tests/integration/test_restore.py
@@ -1,11 +1,31 @@
 from dotenv import load_dotenv
+import traceback
+import shutil
+import sys
 import os
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
+if os.getenv("ENV", "") == "DEBUG": os.environ["EXEC_DIR"] = os.path.dirname(os.path.abspath(__file__))
+
+from vesperando_core import conf
+
+from click.testing import CliRunner
+from vesperando_cli.__main__ import cli
 
 from vesperando_core import packer, configs
 
 
 if __name__ == "__main__":
-    game_dir: str = configs.Settings.get().get('paths', {}).get('game', '')
-    packer.restore_backup(game_dir)
+    print("[TEST] Testing 'restore' command")
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ["restore"])
+
+    print("[TEST] OUTPUT:")
+    print(result.output)
+    if result.exception:
+        traceback.print_exception(*result.exc_info)
+
+    assert result.exit_code == 0
+
+    print("[TEST] Done.")
